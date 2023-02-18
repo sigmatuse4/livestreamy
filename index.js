cb2aed9c0af54b9507d26ffceb646ff349af969c
Rald <56120267+sigmatuse4@users.noreply.github.com>
2023-02-18 20:08:11 +0800

    provide null alternative

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const FFMPEG = require('ffmpeg-static');
 const { spawn } = require('child_process');
 
 if (!process.env.YOUTUBE_RTMP) throw new TypeError(`Your YouTube RTMP url is empty`);
-const spawnConfig = `-stream_loop -1 -re -i video.mp4 -stream_loop -1 -re -i ${process.env.AUDIO_FILENAME} -vcodec libx264 -pix_fmt yuvj420p -maxrate 2048k -preset ultrafast -r 12 -framerate 1 -g 50 -crf 51 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 100 -b:v 500k -f flv ${process.env.YOUTUBE_RTMP}`;
+const spawnConfig = `-stream_loop -1 -re -i video.mp4 -stream_loop -1 -re -i ${process.env.AUDIO_FILENAME || "example.mp3"} -vcodec libx264 -pix_fmt yuvj420p -maxrate 2048k -preset ultrafast -r 12 -framerate 1 -g 50 -crf 51 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 100 -b:v 500k -f flv ${process.env.YOUTUBE_RTMP}`;
 
 const ffmpeg = spawn(FFMPEG, spawnConfig.split(' '));
 
